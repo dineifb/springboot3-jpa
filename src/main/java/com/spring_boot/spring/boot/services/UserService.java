c68448e28b42e8cbbff9e5053e59b98299be477f
Adyrnney <dineifb@yahoo.com.br>
2023-09-26 11:42:40 +0200

    Service Layer and Component Registration

---
+++ src/main/java/com/spring_boot/spring/boot/services/UserService.java
@@ -0,0 +1,30 @@
+package com.spring_boot.spring.boot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring_boot.spring.boot.entities.User;
+import com.spring_boot.spring.boot.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
